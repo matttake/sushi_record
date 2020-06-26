da0e013ca1759910101a8b0dee2bb278cf197814
Yuto Tsukamoto <t.fr.swim@icloud.com>
2020-06-26 23:03:38 +0900

    heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,22 @@
+# 下記の三行は入れていた方がいいらしい
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],#AWSのaccess_key_id
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],#AWSのsecret_access_key
-    region: 'ap-northeast-1' #アジアパシフィック（東京）を表している
-  }
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      provider: 'AWS',
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+    }
+    config.fog_directory     =  ENV['AWS_S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  end
 
-  config.fog_directory  = "sushi-record"
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/sushi-record'
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
